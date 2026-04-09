45d02fadf0de611453ea04a0dedfc66a95987aab
lfisher447-afk <lfisher447@gmail.com>
2026-04-08 19:45:55 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  apps: [
+  apps:[
     {
-      name: 'Movies', // Application name
-      script: './server.js', // Path to your application entry point
-      instances: 'max', // Cluster mode, maximum instances
-      exec_mode: 'cluster', // Enable cluster mode
-      error_file: './logs/err.log', // Error log file
-      out_file: './logs/out.log', // Output log file
-      log_date_format: 'YYYY-MM-DD HH:mm Z', // Log date format
-      autorestart: true, // Enable auto-restart
-      max_memory_restart: '200M', // Memory management, restart when using over 200MB
-      watch: true, // Enable watch mode
-      ignore_watch: ["node_modules", "logs"], // Watch exclusions
-      shutdown_with_force: true, // Graceful shutdown
+      name: 'BingeBox-Omega',
+      script: './api/index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      autorestart: true,
+      max_memory_restart: '500M',
+      watch: false,
+      shutdown_with_force: true,
       env: {
-        NODE_ENV: 'production', // Environment variables
+        NODE_ENV: 'production',
+        PORT: 3000,
+        ENABLE_CLUSTERING: 'true'
       },
     },
   ],
